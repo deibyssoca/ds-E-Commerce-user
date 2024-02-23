5342dabb681f85adfef1d2da8e6b0d15878316dc
deibyssoca <deibyssoca@gmail.com>
2024-02-23 13:17:17 +0100

    Last Commit

--- upload.bat
+++ upload.bat
@@ -4,6 +4,6 @@ git push
 set GOOS=linux
 set GOARCH=amd64
 @REM go build main.go
--tags lambda.norpc -o bootstrap main.go
+go build -tags lambda.norpc -o bootstrap main.go
 del main.zip
 tar.exe -a -cf main.zip bootstrap
